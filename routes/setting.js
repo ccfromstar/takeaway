a3dc1d1f3aa37d203037632c5984c6b14fed633e
Uncle CHEN <114413511@qq.com>
2016-02-21 10:28:10 +0800

    publish

--- routes/setting.js
+++ routes/setting.js
@@ -1,10 +1,7 @@
 module.exports = {
-	
-        host: '117.121.25.131',
-        //host: '127.0.0.1',
+        host: '127.0.0.1',
         user: 'root',
-        password: 'password123',
-        //password: '',
+        password: 'password',
         database:'takaway',
         port: 3306
 	
