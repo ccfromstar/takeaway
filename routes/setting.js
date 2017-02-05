89f3dfa449fd84bf18d19695672e0921301466e6
ccfromstar <114413511@qq.com>
2017-02-05 11:16:02 +0800

    update

--- routes/setting.js
+++ routes/setting.js
@@ -1,7 +1,3 @@
 module.exports = {
-        host: 'rm-uf6100u5egk87us1eo.mysql.rds.aliyuncs.com',
-        user: 'wskroot',
-        password: 'Ws123456',
-        database:'takeaway',
-        port: 3306
+        
 };
